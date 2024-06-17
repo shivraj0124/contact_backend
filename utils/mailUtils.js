545ea553a76f8a7006a5a3deef7865ed7790b615
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2024-06-17 15:58:41 +0530

    Update mailUtils.js

--- utils/mailUtils.js
+++ utils/mailUtils.js
@@ -2,18 +2,25 @@ const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const sendmail = (name, email,message) => {
+const sendmail = (name, email, message) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.email,
+      user: process.env.email, 
       pass: process.env.password,
     },
+    tls: {
+      rejectUnauthorized: false, 
+    },
+    connectionTimeout: 60000, // 60 seconds
+    greetingTimeout: 30000, // 30 seconds
+    socketTimeout: 30000 // 30 seconds
   });
-  let text = "";
-  text = `Name: ${name}, Email: ${email}, Message: ${message}`
+
+  const text = `Name: ${name}, Email: ${email}, Message: ${message}`;
+
   const mailOptions = {
-    from: process.env.email,
+    from: process.env.EMAIL,
     to: "sk40fs@gmail.com",
     subject: "Contact Us",
     text: text,
@@ -21,11 +28,9 @@ const sendmail = (name, email,message) => {
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error(error);
-     
+      console.error("Error occurred:", error.message);
     } else {
       console.log("Email sent: " + info.response);
-     
     }
   });
 };
